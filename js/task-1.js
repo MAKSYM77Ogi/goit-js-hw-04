68f76920089825aa23b089792a20de8ed7e7e70c
MAKSYM77Ogi <ojilba@gmail.com>
2024-05-18 23:07:20 +0200

    goit-js-hw-04

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,12 @@
-function slugify(title) {
+function isEnoughCapacity(products, containerSize) {
+  
+  const totalProducts = Object.values(products).reduce((total, quantity) => total + quantity, 0);
 
-    const lowerCaseTitle = title.toLowerCase();
-    
-    const words = lowerCaseTitle.split(" ");
-    
-    const slug = words.join("-");
-    
-    return slug;
+  return totalProducts <= containerSize;
 }
 
-// Перевірка роботи функції
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developers")); // "english-for-developers"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+// Перевірка функції
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
